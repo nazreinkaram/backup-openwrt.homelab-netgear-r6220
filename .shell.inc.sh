64600f8cb37a9a58a3c188ae06297605cec0afad
Manjeet <manjeet.imaniac@gmail.com>
2023-05-20 11:13:48 +0530

    updated

--- .shell.inc.sh
+++ .shell.inc.sh
@@ -1,10 +1,15 @@
+# Variables
 # Workiing directory will always be /overlay/upper
 WORKING_DIRECTORY="/overlay/upper"
 
-# Temporary git repo name to which remote will be cloned 
+# Temporary git repo name to which remote will be cloned
 # and then .gitrepoconfig will be copied to .git/config
 TEMP_GIT_REPO_NAME="temp-git-repo"
 
+# List of all installed packages
+INSTALLED_PACKAGES_LIST=$(opkg list-installed)
+
+# Functions
 # Wrap new line around text
 say() {
     printf "\n$1\n"
@@ -12,4 +17,22 @@ say() {
 
 wait() {
     sleep 5
+}
+
+install_package() {
+    #
+    say "Installing package '$1'..."
+
+    if [ "$(echo "$INSTALLED_PACKAGES_LIST" | grep "$1")" ]; then
+        say "Package '$1' is already installed. Skipping..."
+    else
+        opkg install "$1"
+
+        if [ $? -eq 0 ]; then
+            say "Package '$1' has been installed successfully."
+        else
+            say "Failed to install package '$1'."
+            exit 1
+        fi
+    fi
 }
--- configure-homelab.sh
+++ configure-homelab.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 SHELL_CONFIG_FILE="$(dirname "$0")/.shell.inc.sh"
 
@@ -33,53 +33,53 @@ cd "$WORKING_DIRECTORY"
 wait
 
 say "Updating package list"
-opkg update
-wait
+# opkg update
+# wait
 
 say "Installing necessary packages one by one"
-opkg install bash
-opkg install nano-full
-opkg install git-http
-opkg install ethtool
-opkg install iptables-nft
-opkg install stubby
-opkg install tailscale
-opkg install luci-app-nlbwmon
-opkg install luci-app-wol
-opkg install ddns-scripts
-opkg install ddns-scripts-cloudflare
-opkg install luci-app-acme
+install_package bash
+install_package nano-full
+install_package git-http
+install_package ethtool
+install_package iptables-nft
+install_package stubby
+install_package tailscale
+install_package luci-app-nlbwmon
+install_package luci-app-wol
+install_package ddns-scripts
+install_package ddns-scripts-cloudflare
+install_package luci-app-acme
 wait
 
-say "Deleting any existing .git directory"
-rm -rf .git
-wait
+# say "Deleting any existing .git directory"
+# rm -rf .git
+# wait
 
-say "Initializing new Git repository"
-git init
-wait
+# say "Initializing new Git repository"
+# git init
+# wait
 
-say "Cloning provided git repository to '$TEMP_GIT_REPO_NAME'"
-git clone "$GIT_URL" "$TEMP_GIT_REPO_NAME"
-wait
+# say "Cloning provided git repository to '$TEMP_GIT_REPO_NAME'"
+# git clone "$GIT_URL" "$TEMP_GIT_REPO_NAME"
+# wait
 
-if [ $? -eq 0 ]; then
+# if [ $? -eq 0 ]; then
 
-    say "Restoring '.git/config' from '$TEMP_GIT_REPO_NAME.gitrepoconfig'"
-    cp "$TEMP_GIT_REPO_NAME.gitrepoconfig" .git/config
-    wait
+#     say "Restoring '.git/config' from '$TEMP_GIT_REPO_NAME.gitrepoconfig'"
+#     cp "$TEMP_GIT_REPO_NAME.gitrepoconfig" .git/config
+#     wait
 
-    say "Removing '$TEMP_GIT_REPO_NAME'"
-    rm -rf "$TEMP_GIT_REPO_NAME"
-    wait
+#     say "Removing '$TEMP_GIT_REPO_NAME'"
+#     rm -rf "$TEMP_GIT_REPO_NAME"
+#     wait
 
-else
+# else
 
-    say "Failed to clone provided git repository. Please check the URL and try again."
+#     say "Failed to clone provided git repository. Please check the URL and try again."
 
-fi
+# fi
 
-# # # Sync with remote
-# # git fetch --all
-# # git reset --hard
-# # git pull
+# # # # Sync with remote
+# # # git fetch --all
+# # # git reset --hard
+# # # git pull
