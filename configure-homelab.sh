a54ce8aa40dd45164e8a0581e98f91958a260d19
Manjeet <manjeet.imaniac@gmail.com>
2023-05-20 11:38:20 +0530

    updated

--- configure-homelab.sh
+++ configure-homelab.sh
@@ -35,32 +35,32 @@ fi
 printf "\n"
 read -p "Enter Backup Repository URL [http(s)]: " GIT_URL
 
-# say "Started homelab configuration"
+say "Started homelab configuration"
+wait
+
+say "Changing directory to: '$WORKING_DIRECTORY'"
+cd "$WORKING_DIRECTORY"
+wait
+
+say "Updating package list"
+# opkg update
 # wait
 
-# say "Changing directory to: '$WORKING_DIRECTORY'"
-# cd "$WORKING_DIRECTORY"
-# wait
+say "Installing necessary packages one by one"
+install_package bash
+install_package nano-full
+install_package git-http
+install_package ethtool
+install_package iptables-nft
+install_package stubby
+install_package tailscale
+install_package luci-app-nlbwmon
+install_package luci-app-wol
+install_package ddns-scripts
+install_package ddns-scripts-cloudflare
+install_package luci-app-acme
 
-# say "Updating package list"
-# # opkg update
-# # wait
-
-# say "Installing necessary packages one by one"
-# install_package bash
-# install_package nano-full
-# install_package git-http
-# install_package ethtool
-# install_package iptables-nft
-# install_package stubby
-# install_package tailscale
-# install_package luci-app-nlbwmon
-# install_package luci-app-wol
-# install_package ddns-scripts
-# install_package ddns-scripts-cloudflare
-# install_package luci-app-acme
-
-# wait
+wait
 
 # # say "Deleting any existing .git directory"
 # # rm -rf .git
