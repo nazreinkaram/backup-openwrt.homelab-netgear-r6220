18c6eaeb528be121246bea0c1f6a8d499d411c85
Manjeet <manjeet.imaniac@gmail.com>
2023-05-20 14:39:50 +0530

    updated

--- configure-homelab.sh
+++ configure-homelab.sh
@@ -98,6 +98,10 @@ wait
 
 if [ "$GIT_CLONE_EXIT_CODE" -eq 0 ]; then
 
+    _say "RESTORING 'git global config' from '$TEMP_GIT_REPO_NAME'"
+    cp "$TEMP_GIT_REPO_NAME/root/.git-credentials" /root/.git-credentials
+    cp "$TEMP_GIT_REPO_NAME/root/.gitconfig" /root/.gitconfig
+
     _say "INITIALIZING new Git repository"
     git init
     wait
